d7a151e0697ad4c17893f623311149fde241f4f8
klequis <klequis@gmail.com>
2019-06-12 14:43:04 -0700

    moved function to lib

--- db/helpers.js
+++ db/helpers.js
@@ -1,8 +1,6 @@
 import { ObjectID } from 'mongodb'
 import { omit } from 'ramda'
 
-const checkForHexString = new RegExp('^[0-9a-fA-F]{24}$')
-
 export const removeIdProp = obj => {
   return omit(['_id'], obj)
 }
