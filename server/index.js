a8b1a89c2b5a527a6152dbf94347e1676acb7595
klequis <klequis@gmail.com>
2019-07-02 14:53:12 -0700

    implement Auth0 authorization

--- server/index.js
+++ server/index.js
@@ -1,17 +1,41 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import helmet from 'helmet'
 import morgan from 'morgan'
 import todo from 'routes/todo-route'
 import config from 'config'
-import { red } from 'logger'
+const jwt = require('express-jwt')
+const jwksRsa = require('jwks-rsa')
+
+import { yellow, red } from 'logger'
+
+yellow('config', config)
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: config.auth0.jwksUri
+  }),
+
+  // Validate the audience and the issuer.
+  audience: config.auth0.appIdentifier,
+  // issuer: `https://<AUTH0_DOMAIN>/`,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  // algorithms: ["RS256"]
+  algorithms: config.auth0.algorithms
+})
 
 const app = express()
 
+app.use(helmet())
 app.use(cors())
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 
+app.use(checkJwt)
 app.use('/api/todo', todo)
 app.use(function(req, res, next) {
   res.status(404).send('Unknown endpoint')
