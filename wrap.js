3b854d926087e6ce98b97babe52cdd91c3df6c51
klequis <klequis@gmail.com>
2019-08-04 17:34:32 -0700

    temporary versions of wrap function

--- wrap.js
+++ wrap.js
@@ -1,3 +1,27 @@
+// https://strongloop.com/strongblog/async-error-handling-expressjs-es7-promises-generators/#cleaner-code-with-generators
+
+import { yellow, green } from 'logger'
+
 const wrap = fn => (...args) => fn(...args).catch(args[2])
+
+const w1 = fn => (...args) => {
+  // yellow('wrap called with args', args)
+  yellow('fn', fn)
+  // yellow('args[0]', args[0]) // response 'IncomingMessage'
+  // yellow('args[1]', args[1]) // response 'ServerResponse'
+  // yellow('args[2]', args[2]) // next()
+  fn.catch(e => {
+    yellow('wrap: e', e)
+  })
+}
+
+const w2 = fn => (...args) =>
+  fn(...args).catch(() => {
+    green('length', args.length)
+    // green('args[0]', args[0])
+    // green('args[1]', args[1]) // response
+    // green('args[2]', args[2]) // next()
+    args[2](args[0])
+  })
 
 export default wrap
