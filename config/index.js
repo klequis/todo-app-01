d9e5a043db929de4f6982de77774f5d38d5af01b
klequis <klequis@gmail.com>
2019-08-04 17:31:02 -0700

    Use logger instead of console.log

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 // settings are in keybase
 
 import settings from './config.settings'
-import { yellow } from '../logger'
+import { bluef } from '../logger'
 
 const TEST_LOCAL = 'testLocal'
 const TEST_REMOTE = 'testRemote'
@@ -12,23 +12,20 @@ const PROD = 'production'
 const unknowEnvName = (env) => `ERROR: config/index.js: unknown environment name: ${env}. Must be ${TEST_LOCAL}, ${TEST_REMOTE}, ${DEV} or ${PROD}`
 
 export const mongoUri = env => {
-  // yellow('env', env)
-  // yellow('settings', settings)
   switch (env) {
     case TEST_LOCAL:
-      console.log('env: ', env)
-      console.log('monguUri: ', settings.db.testLocal.mongoUri)
+      bluef('env: ', env)
+      bluef('monguUri: ', settings.db.testLocal.mongoUri)
       return settings.db.testLocal.mongoUri
     case TEST_REMOTE:
-      console.log('env: ', env)
-      console.log('monguUri: ', settings.db.testRemote.mongoUri)
+      bluef('env: ', env)
+      bluef('monguUri: ', settings.db.testRemote.mongoUri)
       return settings.db.testRemote.mongoUri
     case DEV:
-      console.log('env: ', env)
-      console.log('monguUri: ', settings.db.development.mongoUri)
+      bluef('env: ', env)
+      bluef('monguUri: ', settings.db.development.mongoUri)
       return settings.db.development.mongoUri
     case PROD:
-      console.log('settings.db.production.mongoUri', settings.db.production.mongoUri)
       return settings.db.production.mongoUri
     default:
       throw new Error(unknowEnvName())
@@ -51,7 +48,7 @@ export const dbName = env => {
 }
 
 export const apiRoot = (env)  => {
-  console.log('apiRoot: env', env);
+  bluef('apiRoot: env', env);
   
   switch (env) {
     case TEST_LOCAL:
