cff5cc0272b428bc0a31fce6f4384ce8bc33037a
klequis <klequis@gmail.com>
2019-08-06 07:16:48 -0700

    clean-up

--- test/config/config.test.js
+++ test/config/config.test.js
@@ -1,5 +1,4 @@
 import { expect } from 'chai'
-import { yellow } from 'logger'
 import { TEST_LOCAL, TEST_REMOTE, DEV, PROD } from 'config'
 import config from 'config'
 import settings from '../../config/config.settings'
@@ -10,31 +9,27 @@ describe('test config', function() {
   environments.forEach(env => {
     let cfg
     before(function() {
-      // yellow('sending', env)
       cfg = config(env)
     })
-    describe(`test config for ${env}`, function() {
 
+    describe(`test config for ${env}`, function() {
       it('should return apiRoot', function() {
-        if (env in[TEST_LOCAL, DEV, TEST_LOCAL]) {
+        if (env in [TEST_LOCAL, DEV, TEST_LOCAL]) {
           expect(cfg.apiRoot).to.equal(settings.apiRoot.local)
         }
         if (env === PROD) {
           expect(cfg.apiRoot).to.equal(settings.apiRoot.remote)
         }
       })
-
       it('should return 7', function() {
         // decided to only do this test to auth0 data to keep
         // data concealed.
         // Isn't env dependent but should be the same in each environment
         expect(Object.keys(cfg.auth0).length).to.equal(7)
-      })  
-
+      })
       it('should = production', function() {
         expect(cfg.env).to.equal(env)
       })
-
       it('should return server port', function() {
         if (env in [TEST_LOCAL, DEV, TEST_LOCAL]) {
           expect(cfg.port).to.equal(settings.serverPort.local)
@@ -43,12 +38,7 @@ describe('test config', function() {
           expect(cfg.port).to.equal(settings.serverPort.remote)
         }
       })
-
-      
-
     })
-
-
   })
 
   describe('config should throw', function() {
@@ -56,8 +46,4 @@ describe('test config', function() {
       expect(() => config('junk')).to.throw()
     })
   })
-
-  
-  
-
 })
