551a0db0be57bbbd4b3572523c2c55cd34d7a2c3
klequis <klequis@gmail.com>
2019-08-07 08:06:32 -0700

    1) use new todos in fixture, 2) use sendRequest()

--- test/routes/todo/todo-route-post.test.js
+++ test/routes/todo/todo-route-post.test.js
@@ -1,27 +1,28 @@
 import { expect } from 'chai'
 import request from 'supertest'
 import { equals } from 'ramda'
-import { fourTodos, oneTodo } from './fixture'
+import {
+  goodTodo,
+  missingEmailTodo,
+  invalidEmailTodo01,
+  invalidEmailTodo02,
+  missingTitleTodo,
+  titleTooShortTodo,
+  codeTitleTodo,
+  emptyTitleTodo
+} from './fixture'
 import app from 'server'
 import { dropCollection } from 'db'
 import getToken from 'test/get-token'
+import sendRequest from '../../send-request'
 
 const collectionName = 'todos'
 
-const titleTypeErr = {
-  location: 'body',
-  param: 'title',
-  msg: 'Title must be a string.'
-}
+const titleTooShortMsg = 'Title must be at least 3 characters long.'
 
-const titleLenErr = {
-  location: 'body',
-  param: 'title',
-  msg: 'Title must be at least 3 characters long.'
-}
+const invalidEmailMsg = 'Invalid or missing email.'
 
-describe('todo-route POST', function() {
-
+describe.only('todo-route POST', function() {
   let token = undefined
 
   before(async function() {
@@ -32,93 +33,48 @@ describe('todo-route POST', function() {
     before(async function() {
       await dropCollection(collectionName)
     })
+
     it('should post 1 todo', async function() {
-      const r = await request(app)
-        .post('/api/todo')
-        .set('Accept', 'application/json')
-        .set('Authorization', `Bearer ${token.access_token}`)
-        .send(oneTodo)
-        .expect('Content-Type', /json/)
-        .expect(200)
+      const r = await sendRequest(goodTodo, 200, token)
       const data = r.body[0]
-      expect(data.title).to.equal(oneTodo.title)
+      expect(data.title).to.equal(goodTodo.title)
       expect(data.completed).to.equal(false)
+      expect(data.email).to.equal(goodTodo.email)
     })
+
+    it('should fail validation - missing email', async function() {
+      const r = await sendRequest(missingEmailTodo, 422, token)
+      const { errors } = r.body
+      expect(errors[0].msg).to.equal(invalidEmailMsg)
+    })
+
+    it('should fail validation - invalid email missing tld', async function() {
+      const r = await sendRequest(invalidEmailTodo01, 422, token)
+      const { errors } = r.body
+      expect(errors[0].msg).to.equal(invalidEmailMsg)
+    })
+    it('should fail validation - invalid email missing @', async function() {
+      const r = await sendRequest(invalidEmailTodo02, 422, token)
+      const { errors } = r.body
+      expect(errors[0].msg).to.equal(invalidEmailMsg)
+    })
+    it('should fail validation - missing title', async function() {
+      const r = await sendRequest(missingTitleTodo, 422, token)
+      const { errors } = r.body
+      expect(errors[0].msg).to.equal(titleTooShortMsg)
+    })
+    it('should fail validation - title too short', async function() {
+      const r = await sendRequest(titleTooShortTodo, 422, token)
+      const { errors } = r.body
+      expect(errors[0].msg).to.equal(titleTooShortMsg)
+    })
+    it('should fail validation - empty title', async function() {
+      const r = await sendRequest(emptyTitleTodo, 422, token)
+      const { errors } = r.body
+      expect(errors[0].msg).to.equal(titleTooShortMsg)
+    })
+
+
   })
 
-  describe('test validation for POST /api/todo', function() {
-    it('send it nothing at all', async function() {
-      const r = await request(app)
-        .post('/api/todo')
-        .set('Accept', 'application/json')
-        .set('Authorization', `Bearer ${token.access_token}`)
-        .send()
-      const { errors } = r.body
-      expect(errors.length).to.equal(2)
-      expect(equals(errors[0], titleTypeErr)).to.equal(true)
-      expect(equals(errors[1], titleLenErr)).to.equal(true)
-    })
-    it('send "" ', async function() {
-      const r = await request(app)
-        .post('/api/todo')
-        .set('Accept', 'application/json')
-        .set('Authorization', `Bearer ${token.access_token}`)
-        .send('')
-      const { errors } = r.body
-      expect(errors.length).to.equal(2)
-      expect(equals(errors[0], titleTypeErr)).to.equal(true)
-      expect(equals(errors[1], titleLenErr)).to.equal(true)
-    })
-    it('send "a" ', async function() {
-      const r = await request(app)
-        .post('/api/todo')
-        .set('Accept', 'application/json')
-        .set('Authorization', `Bearer ${token.access_token}`)
-        .send('a')
-      const { errors } = r.body
-      expect(errors.length).to.equal(2)
-      expect(equals(errors[0], titleTypeErr)).to.equal(true)
-      expect(equals(errors[1], titleLenErr)).to.equal(true)
-    })
-    it('send {}', async function() {
-      const r = await request(app)
-        .post('/api/todo')
-        .set('Accept', 'application/json')
-        .set('Authorization', `Bearer ${token.access_token}`)
-        .send({})
-      const { errors } = r.body
-      expect(errors.length).to.equal(2)
-      expect(equals(errors[0], titleTypeErr)).to.equal(true)
-      expect(equals(errors[1], titleLenErr)).to.equal(true)
-    })
-    it('send too short title', async function() {
-      const r = await request(app)
-        .post('/api/todo')
-        .set('Accept', 'application/json')
-        .set('Authorization', `Bearer ${token.access_token}`)
-        .send({ title: 'a' })
-      const { errors } = r.body
-      expect(errors.length).to.equal(1)
-      expect(errors[0].msg).to.equal(titleLenErr.msg)
-    })
-    it('send title = 123', async function() {
-      const r = await request(app)
-        .post('/api/todo')
-        .set('Accept', 'application/json')
-        .set('Authorization', `Bearer ${token.access_token}`)
-        .send({ title: 123 })
-      const { errors } = r.body
-      expect(errors[0].msg).to.equal(titleTypeErr.msg)
-    })
-    it('send title = "123" ', async function() {
-      const r = await request(app)
-        .post('/api/todo')
-        .set('Accept', 'application/json')
-        .set('Authorization', `Bearer ${token.access_token}`)
-        .send({ title: '123' })
-      const { body } = r
-      const data = body[0]
-      expect(data.title).to.equal('123')
-    })
-  })
 })
