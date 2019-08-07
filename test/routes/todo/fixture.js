3ed5c6c9aec37326e21adbe72c257d8156b33aa4
klequis <klequis@gmail.com>
2019-08-07 08:05:51 -0700

    more todos added

--- test/routes/todo/fixture.js
+++ test/routes/todo/fixture.js
@@ -17,7 +17,36 @@ export const fourTodos = [
   }
 ]
 
-export const oneTodo = {
-  title: 'one single todo',
-  completed: false
+
+// goodTodo
+export const goodTodo = {
+  email: 'testUser@test.com',
+  title: 'a good todo'
+}
+// missingEmailTodo
+export const missingEmailTodo = {
+  title: 'a good todo'
+}
+// invalidEmailTodo
+export const invalidEmailTodo01 = {
+  email: 'testUser@test',
+  title: 'a good todo'
+}
+export const invalidEmailTodo02 = {
+  email: 'testUsertest.com',
+  title: 'a good todo'
+}
+// missingTitleTodo
+export const missingTitleTodo = {
+  email: 'testUser@test.com'
+}
+// titleTooShort
+export const titleTooShortTodo = {
+  email: 'testUser@test.com',
+  title: 'ab'
+}
+// emptyTitle
+export const emptyTitleTodo = {
+  email: 'testUser@test.com',
+  title: ''
 }
