221368ff80683608607c0a146e874d8600e2ab8e
klequis <klequis@gmail.com>
2019-08-07 13:29:16 -0700

    clean-up

--- test/routes/todo/todo-route-post.test.js
+++ test/routes/todo/todo-route-post.test.js
@@ -14,7 +14,7 @@ import {
 import app from 'server'
 import { dropCollection } from 'db'
 import getToken from 'test/get-token'
-import sendRequest from '../../send-request'
+import sendRequest from 'test/sendRequest'
 
 const collectionName = 'todos'
 
@@ -22,7 +22,9 @@ const titleTooShortMsg = 'Title must be at least 3 characters long.'
 
 const invalidEmailMsg = 'Invalid or missing email.'
 
-describe.only('todo-route POST', function() {
+const postUri = '/api/todo'
+
+describe('todo-route POST', function() {
   let token = undefined
 
   before(async function() {
@@ -35,7 +37,13 @@ describe.only('todo-route POST', function() {
     })
 
     it('should post 1 todo', async function() {
-      const r = await sendRequest(goodTodo, 200, token)
+      const r = await sendRequest({
+        method: 'POST',
+        uri: postUri,
+        status: 200,
+        body: goodTodo,
+        token
+      })
       const data = r.body[0]
       expect(data.title).to.equal(goodTodo.title)
       expect(data.completed).to.equal(false)
@@ -43,38 +51,70 @@ describe.only('todo-route POST', function() {
     })
 
     it('should fail validation - missing email', async function() {
-      const r = await sendRequest(missingEmailTodo, 422, token)
+      const r = await sendRequest({
+        method: 'POST',
+        uri: postUri,
+        status: 422,
+        body: missingEmailTodo,
+        token
+      })
       const { errors } = r.body
       expect(errors[0].msg).to.equal(invalidEmailMsg)
     })
 
     it('should fail validation - invalid email missing tld', async function() {
-      const r = await sendRequest(invalidEmailTodo01, 422, token)
+      const r = await sendRequest({
+        method: 'POST', 
+        uri: postUri,
+        status: 422,
+        body: invalidEmailTodo01, 
+        token
+      })
       const { errors } = r.body
       expect(errors[0].msg).to.equal(invalidEmailMsg)
     })
     it('should fail validation - invalid email missing @', async function() {
-      const r = await sendRequest(invalidEmailTodo02, 422, token)
+      const r = await sendRequest({
+        method: 'POST',
+        uri: postUri,
+        status: 422,
+        body: invalidEmailTodo02,
+        token
+      })
       const { errors } = r.body
       expect(errors[0].msg).to.equal(invalidEmailMsg)
     })
     it('should fail validation - missing title', async function() {
-      const r = await sendRequest(missingTitleTodo, 422, token)
+      const r = await sendRequest({
+        method: 'POST', 
+        uri: postUri,
+        status: 422,
+        body: missingTitleTodo,
+        token
+      })
       const { errors } = r.body
       expect(errors[0].msg).to.equal(titleTooShortMsg)
     })
     it('should fail validation - title too short', async function() {
-      const r = await sendRequest(titleTooShortTodo, 422, token)
+      const r = await sendRequest({
+        method: 'POST', 
+        uri: postUri,
+        status: 422,
+        body: titleTooShortTodo,
+        token
+      })
       const { errors } = r.body
       expect(errors[0].msg).to.equal(titleTooShortMsg)
     })
     it('should fail validation - empty title', async function() {
-      const r = await sendRequest(emptyTitleTodo, 422, token)
+      const r = await sendRequest({
+        method: 'POST', 
+        uri: postUri,
+        status: 422,
+        body: emptyTitleTodo,
+        token})
       const { errors } = r.body
       expect(errors[0].msg).to.equal(titleTooShortMsg)
     })
-
-
   })
-
 })
