205ef78e38e35d9b1cfa73dcd9a9c4b06ab639ec
klequis <klequis@gmail.com>
2019-08-12 15:55:30 -0700

    rewriting tests - wip

--- test/routes/todo/todo-route-patch.test..js
+++ test/routes/todo/todo-route-patch.test..js
@@ -1,15 +1,20 @@
 import { expect } from 'chai'
-import { fourTodos } from './fixture'
+import { fourTodosForPost } from './fixture'
 import { dropCollection, insertMany } from 'db'
 import getToken from 'test/get-token'
 import sendRequest from 'test/sendRequest'
 import { TODO_COLLECTION_NAME } from 'routes/constants'
+import { mergeRight } from 'ramda'
+import { yellow } from 'logger'
+import { isDateTimeAfter } from 'lib'
 
-const patchUri = `/api/todo`
+function patchUri(id) {
+  return `/api/todo/${id}`
+}
 
-describe('todo-route', function() {
+describe.only('todo-route PATCH', function() {
   let token = undefined
-
+  yellow('4todos', fourTodosForPost)
   before(async function() {
     token = await getToken()
   })
@@ -18,36 +23,56 @@ describe('todo-route', function() {
     let idToUpdate2
     before(async function() {
       await dropCollection(TODO_COLLECTION_NAME)
-      const inserted = await insertMany(TODO_COLLECTION_NAME, fourTodos)
+      const inserted = await insertMany(TODO_COLLECTION_NAME, fourTodosForPost)
       idToUpdate1 = inserted[1]._id.toString()
       idToUpdate2 = inserted[2]._id.toString()
     })
-    it('update title and completed should return document with updated title & completed', async function() {
-      const newData = {
+    it.only('all fields valid - should return document with updated title, completed & lastUpdateAt', async function() {
+      // use fourTodosForPost[1]
+      const originalTodo = fourTodosForPost[1]
+      const newData = mergeRight(originalTodo, {
         _id: idToUpdate1,
         title: 'changed title',
         completed: true
-      }
+      })
       const r = await sendRequest({
         method: 'PATCH',
-        uri: patchUri,
+        uri: patchUri(newData._id),
         status: 200,
         body: newData,
-        token,
+        token
       })
-      const data = r.body[0]
-      expect(data.title).to.equal(newData.title)
-      expect(data.completed).to.equal(true)
+      const { body } = r
+      expect(body.length).to.equal(1)
+      const modifiedTodo = body[0]
+      expect(modifiedTodo._id).to.equal(idToUpdate1)
+      expect(modifiedTodo.completed).to.equal(true)
+
+      // yellow('todo.lastUpdatedAt', modifiedTodo.lastUpdatedAt)
+
+      const origDate = fourTodosForPost[1].lastUpdatedAt
+      yellow('origDate', origDate)
+      const modDate = modifiedTodo.lastUpdatedAt
+      yellow('modDate', modDate)
+      // isAfter - is the first date after the second date
+
+
+      // origDate  2019-08-12T17:01:16.927Z
+      //  modDate  2019-08-12T22:26:28.378Z
+
+      const after = isDateTimeAfter(origDate, modDate)
+      expect(after).to.equal(true)
+      expect(modifiedTodo.title).to.equal(newData.title)
     })
 
     it('should return document with updated title only', async function() {
       const newData = { _id: idToUpdate2, title: 'another changed title' }
       const r = await sendRequest({
         method: 'PATCH',
-        uri: patchUri,
+        uri: patchUri(newData._id),
         status: 200,
         body: newData,
-        token,
+        token
       })
       const { body } = r
       expect(body.length).to.equal(1)
