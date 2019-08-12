bbd3b85bd5f5b50afb06fa9b5a7e2203a22b564f
klequis <klequis@gmail.com>
2019-08-12 15:53:12 -0700

    disable checkSchema for patch

--- routes/todo-route/index.js
+++ routes/todo-route/index.js
@@ -3,6 +3,7 @@ import todoGet from './todoGet'
 import todoPost, { postValidationSchema } from './todoPost'
 import todoDelete, { deleteValidationSchema } from './todoDelete'
 import todoGetById, { getByIdValidationSchema } from './todoGetById'
+import todoPatch, { patchValidationSchema } from './todoPatch'
 import { checkSchema } from 'express-validator'
 
 
@@ -12,6 +13,8 @@ router.get('/', todoGet)
 router.post('/', checkSchema(postValidationSchema), todoPost)
 router.delete('/:id', checkSchema(deleteValidationSchema), todoDelete)
 router.get('/:id', checkSchema(getByIdValidationSchema), todoGetById)
+// router.patch('/:id', checkSchema(patchValidationSchema), todoPatch)
+router.patch('/:id', todoPatch)
 
 export default router
 
